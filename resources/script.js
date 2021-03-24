7cde4f7d2cf19e5791e20314173307783288cd0e
Olumide Koyenikan <olumide.koyenikan@yahoo.com>
2021-03-24 16:43:21 +0100

    first commit

---
+++ resources/script.js
@@ -0,0 +1,50 @@
+/*--menu show y hidden--*/
+const navMenu = document.getElementById('nav-menu'),
+		toggleMenu= document.getElementById('nav-toggle'),
+		closeMenu = document.getElementById('nav-close')
+
+		//show
+		toggleMenu.addEventListener('click', ()=>{
+			navMenu.classList.toggle('show')
+		})
+		
+		//hidden
+		closeMenu.addEventListener('click', ()=>{
+			navMenu.classList.toggle('show')
+			
+		})
+		
+		
+		//remove menu
+		const navLink = document.querySelectorAll('.nav__link')
+		function linkAction(){
+			navMenu.classList.toggle('show')
+		}
+		navLink.forEach(n=> n.addEventListener('click', linkAction))
+		
+		
+		
+		//scroll sections
+		const sections= document.querySelectorAll('section[id]')
+		console.log(sections);
+		
+		window.addEventListener('scroll',scrollActive)
+		
+		function scrollActive(){ 
+		const scrollY = window.pageYOffset
+		
+		sections.forEach(current => {
+			const sectionHeight = current.offsetHeight
+			const sectionTop = current.offsetTop - 50
+			sectionId = current.getAttribute('id')
+			console.log(sectionId);
+			
+			if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			document.querySelector('.nav__menu a[href*='+ sectionId + ']').classList.add('active')
+			}
+			else{
+			document.querySelector('.nav__menu a[href*='+ sectionId + ']').classList.remove('active')
+			}
+		})
+		
+		}
